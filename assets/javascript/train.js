87651f9ec136424f35a2b8493154e8b9b2cde21b
Cody Chase <Cody Chase>
2017-06-16 14:22:42 -0400

    added database manipulation

---
+++ assets/javascript/train.js
@@ -0,0 +1,58 @@
+
+//variables
+var initialName = []; 
+var initialDestination = [];
+var initialStartTime = [];
+var initialFrequency = [];
+    
+var trainName = initialName; 
+var destination = initialDestination;
+var startTime = initialStartTime;
+var frequency = initialFrequency;
+    
+    
+var database = firebase.database();
+    
+    
+    database.ref().on("value", function(snapshot){
+        $("#tableBody").empty();
+        
+        if (snapshot.child("name").exists() && snapshot.child("destination").exists() && snapshot.child("startTime").exists() && snapshot.child("frequency").exists()) {
+
+            // Set the local variables for highBidder equal to the stored values in firebase.
+            trainName = snapshot.val().name;
+            destination = snapshot.val().destination;
+            startTime = snapshot.val().startTime;
+            frequency = snapshot.val().frequency;
+            
+            for(var i = 0; i < trainName.length; i++)
+                {
+                    $("#tableBody").append("<tr><td>" + snapshot.val().name[i] + "</td><td>" + snapshot.val().destination[i] + "</td><td>" + snapshot.val().frequency[i] + "</td><td> coming soon </td><td> coming soon </td> </tr>");
+                }
+        }else{
+            $("#tableBody").append("<tr><td>There</td><td>are</td><td>no</td><td>trains</td><td>currently</td> </tr>");
+        }
+    });
+//onClick of add train form
+$("#submit").on("click",function(event){
+    //prevent page reload
+    event.preventDefault();
+    //set variables to the values of the user input
+    trainName.push($("#name").val().trim());
+    destination.push($("#destination").val().trim());
+    startTime.push($("#startTime").val().trim());
+    frequency.push($("#frequency").val().trim());
+    
+    console.log(trainName + " " + destination + " " + startTime + " " + frequency);
+    
+    database.ref().set({
+        name: trainName,
+        destination: destination,
+        startTime: startTime,
+        frequency: frequency
+    })
+    
+});
+    
+    
+ 
