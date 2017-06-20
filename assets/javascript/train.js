020afd895678ddc1dba1567937c2cd4c11c9f57d
Cody Chase <Cody Chase>
2017-06-19 21:23:15 -0400

    finished

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -9,30 +9,43 @@ var trainName = initialName;
 var destination = initialDestination;
 var startTime = initialStartTime;
 var frequency = initialFrequency;
-    
-    
-var database = firebase.database();
-    
-    
-    database.ref().on("value", function(snapshot){
-        $("#tableBody").empty();
         
-        if (snapshot.child("name").exists() && snapshot.child("destination").exists() && snapshot.child("startTime").exists() && snapshot.child("frequency").exists()) {
+var database = firebase.database();
 
-            // Set the local variables for highBidder equal to the stored values in firebase.
-            trainName = snapshot.val().name;
-            destination = snapshot.val().destination;
-            startTime = snapshot.val().startTime;
-            frequency = snapshot.val().frequency;
+var holdTime;
+    
+database.ref().on("value", function(snapshot){
+    $("#tableBody").empty();
+        
+    if (snapshot.child("name").exists() && snapshot.child("destination").exists() && snapshot.child("startTime").exists() && snapshot.child("frequency").exists()) {
+
+    // Set the local variables for highBidder equal to the stored values in firebase.
+        trainName = snapshot.val().name;
+        destination = snapshot.val().destination;
+        startTime = snapshot.val().startTime;
+        frequency = snapshot.val().frequency;
+        
+        
+        var nextTrain = "0";
             
-            for(var i = 0; i < trainName.length; i++)
-                {
-                    $("#tableBody").append("<tr><td>" + snapshot.val().name[i] + "</td><td>" + snapshot.val().destination[i] + "</td><td>" + snapshot.val().frequency[i] + "</td><td> coming soon </td><td> coming soon </td> </tr>");
-                }
-        }else{
-            $("#tableBody").append("<tr><td>There</td><td>are</td><td>no</td><td>trains</td><td>currently</td> </tr>");
-        }
-    });
+        for(var i = 0; i < trainName.length; i++)
+            {
+                holdTime = moment(startTime[i], "HH:mm");
+                var holdTime2 = moment("03:00","HH:mm");
+                var duration = 0;
+                duration = holdTime.diff(moment(),'minutes');
+                var durationPositive = Math.abs(duration);
+                var minTill = frequency[i] - (durationPositive % frequency[i]);
+                var nextArrival = moment().add(minTill, "minutes").format("HH:mm");
+                
+                $("#tableBody").append("<tr><td>" + snapshot.val().name[i] + "</td><td>" + snapshot.val().destination[i] + "</td><td>" + snapshot.val().frequency[i] + "</td><td>" + nextArrival + "</td><td>" + minTill + "</td> </tr>");
+            }
+    }else{
+        
+        
+        $("#tableBody").append("<tr><td>There</td><td>are</td><td>no</td><td>trains</td><td>currently</td> </tr>");
+    }
+});
 //onClick of add train form
 $("#submit").on("click",function(event){
     //prevent page reload
@@ -53,6 +66,3 @@ $("#submit").on("click",function(event){
     })
     
 });
-    
-    
- 
